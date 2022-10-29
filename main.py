60645406abafad966da98f4c781df33a0803695e
Fr0kes <85014298+StepWolf45@users.noreply.github.com>
2022-10-29 22:39:56 +0300

    Update main.py
    New methods

--- main.py
+++ main.py
@@ -11,9 +11,21 @@ class Student:
         self.grade = grade
         self.marks = marks
     def show(self):
-        data = f'{self.name} {self.surname} {self.age} {self.grade}\n {len(self.marks)}'
+        data = f'{self.name} {self.surname} {self.age} {self.grade} {len(self.marks)}'
         marks = " ".join(list(map(str, self.marks)))
         print(f'{data}\n{marks}')
+    def get_average_mark(self):
+        res = 0
+        for i in range(len(self.marks)):
+            res+=self.marks[i]
+        print('%.6f' % (res/len(self.marks)))
+    def will_graduate(self, after_years):
+        if self.grade+after_years>11:
+            return True
+        return False
+    def age_enterance(self):
+        school_age = self.age - self.grade +1
+        print(school_age)
 def read_student():
     data = input().split()
     name, surname = data[0], data[1]
@@ -27,9 +39,11 @@ def main():
     journal = []
     for i in range(5):
          journal.append(read_student())
-    print_all_students(journal)
+         if i!=4:
+             pust = input()
+    for student in journal:
+        student.age_enterance()
 def check_names():
-    """Проверяем наличие нужных классов и методов в программе"""
     names = [
         {'name': 'Student', 'type': 'класс'},
         {'name': 'read_student', 'type': 'метод'},
@@ -39,7 +53,6 @@ def check_names():
         if item['name'] not in globals().keys():
             raise RuntimeError(f"Ошибка: в программе не определён {item['type']} {item['name']}")
 def check_types():
-    """Проверяем соответствие типов данных"""
     type_settings = [
         {'name': 'Student', 'type': 'isclass', 'typename': 'класс'},
         {'name': 'read_student', 'type': 'isfunction', 'typename': 'метод'},
@@ -50,20 +63,17 @@ def check_types():
         if not method(globals()[item['name']]):
             raise RuntimeError(f"Ошибка: {item['name']} не является {item['typename']}ом")
 def check_student_defaults():
-    """Проверяем, что у конструктора класса Student есть параметры по умолчанию"""
     try:
         Student()
     except TypeError:
         raise RuntimeError('Ошибка: в конструкторе класса Student не указаны значения по умолчанию для всех параметров')
 def check_student_show():
-    """Проверяем, что у класса Student есть метод show"""
     s = Student('Bill', 'Gates', 42, 11, [5, 5, 5])
     if not hasattr(s, 'show'):
         raise RuntimeError('Ошибка: в классе Student нет метода show')
     if not inspect.ismethod(s.show):
         raise RuntimeError('Ошибка: поле show класса Student не является методом')
 def check_print_all_students():
-    """Проверяем корректность параметров метода print_all_students"""
     try:
         a = print_all_students
     except NameError:
